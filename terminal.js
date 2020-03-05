e10d94db62130aede1de0a04d82b14b5805a72df
Nik-Topel <60180879+Nik-Topel@users.noreply.github.com>
2020-03-05 10:33:59 +0100

    Add files via upload

---
+++ terminal.js
@@ -0,0 +1,35 @@
+document.onkeypress = function(evt) {
+    evt = evt || window.event;
+    var charCode = evt.keyCode || evt.which;
+    var charStr = String.fromCharCode(charCode);
+    document.getElementById('write_out').textContent += charStr;
+};
+(function (global) { 
+    if(typeof (global) === "undefined") {
+        throw new Error("window is undefined");
+    }
+    var _hash = "!";
+    var noBackPlease = function () {
+        global.location.href += "#";
+        global.setTimeout(function () {
+            global.location.href += "!";
+        }, 50);
+    };
+
+    global.onhashchange = function () {
+        if (global.location.hash !== _hash) {
+            global.location.hash = _hash;
+        }
+    };
+
+    global.onload = function () {            
+        noBackPlease();
+        document.body.onkeydown = function (e) {
+            var elm = e.target.nodeName.toLowerCase();
+            if (e.which === 8 && (elm !== 'input' && elm  !== 'textarea')) {
+                e.preventDefault();
+            }
+            e.stopPropagation();
+        };          
+    }
+})(window);
