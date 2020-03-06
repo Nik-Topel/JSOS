4ac9d06b4300ea8a96d3725daf90b0cf7b1a30f8
Nik-Topel <niktopel@gmail.com>
2020-03-06 08:30:41 +0100

    update

---
+++ index.js
@@ -0,0 +1,88 @@
+var backspaceCode = 8
+
+    // See http://www.w3schools.com/html/html_form_input_types.asp
+    // See https://github.com/slorber/backspace-disabler/issues/1
+    var validInputTypes = {
+        TEXT:1,PASSWORD:1,FILE:1,EMAIL:1,SEARCH:1,DATE:1,NUMBER:1,
+        MONTH:1,WEEK:1,TIME:1,DATETIME:1,'DATETIME-LOCAL':1,TEL:1,URL:1
+    }
+
+    // Disables the backspace from triggering the browser to go back one history item (which often changes pages)
+    exports.disable = function(el) {
+        addHandler(el || document,"keydown",disabler)
+    }
+
+    // Reenable the browser backs
+    exports.enable = function(el) {
+        removeHandler(el || document,"keydown",disabler)
+    }
+
+
+    // See http://stackoverflow.com/questions/1495219/how-can-i-prevent-the-backspace-key-from-navigating-back
+    function disabler(event) {
+        if (event.keyCode === backspaceCode) {
+            var node = event.srcElement || event.target
+            // We don't want to disable the ability to delete content in form inputs and contenteditables
+            if ( !isActiveFormItem(node) ) {
+                event.preventDefault()
+            }
+        }
+    }
+
+    // See http://stackoverflow.com/questions/12949590/how-to-detach-event-in-ie-6-7-8-9-using-javascript
+    function addHandler(element, type, handler) {
+        if (element.addEventListener) {
+            element.addEventListener(type, handler, false)
+        } else if (element.attachEvent) {
+            element.attachEvent("on" + type, handler)
+        } else {
+            element["on" + type] = handler
+        }
+    }
+    function removeHandler(element, type, handler) {
+        if (element.removeEventListener) {
+            element.removeEventListener(type, handler, false)
+        } else if (element.detachEvent) {
+            element.detachEvent("on" + type, handler)
+        } else {
+            element["on" + type] = null
+        }
+    }
+
+    // Returns true if the node is or is inside an active contenteditable
+    function isInActiveContentEditable(node) {
+        while (node) {
+            if ( node.getAttribute &&
+                 node.getAttribute("contenteditable") &&
+                 node.getAttribute("contenteditable").toUpperCase() === "TRUE" ) {
+                return true
+            }
+
+            node = node.parentNode
+        }
+
+        return false
+    }
+
+    // returns true if the element is contained within a document
+    function connectedToTheDom(node) {
+      // IE does not have contains method on document element, only body   
+      var container = node.ownerDocument.contains ? node.ownerDocument : node.ownerDocument.body;
+      return container.contains(node);
+    }
+
+    function isActiveFormItem(node) {
+        var tagName = node.tagName.toUpperCase()
+        var isInput = ( tagName === "INPUT" && node.type.toUpperCase() in validInputTypes)
+        var isTextarea = ( tagName === "TEXTAREA" )
+        if ( isInput || isTextarea ) {
+            var isDisabled = node.readOnly || node.disabled
+            return !isDisabled && connectedToTheDom(node)  // the element may have been disconnected from the dom between the event happening and the end of the event chain, which is another case that triggers history changes
+        }
+        else if ( isInActiveContentEditable(node) ) {
+            return connectedToTheDom(node)
+        }
+        else {
+            return false
+        }
+    }
--- terminal.js
+++ terminal.js
@@ -1,37 +1,105 @@
-var c_p = 0
-
+var c_p = 0;
+var words = null;
+var errored_word = 0;// 0 = none 1 = first word etc
 document.onkeypress = function(evt) {
     evt = evt || window.event;
-    var charCode = evt.keyCode || evt.which;
+    var charCode = evt.which;
     var charStr = String.fromCharCode(charCode);
-    document.getElementById('write_out').textContent = document.getElementById('write_out').textContent.substr (0,c_p)+charStr+ document.getElementById('write_out').textContent.substr(c_p, document.getElementById('write_out').textContent.prototype.length) 
+	if (charCode != 8 && charCode != 13 && charCode != 32){
+		if(document.getElementById('write_in').innerHTML == "&nbsp;"){
+			document.getElementById('write_in').innerHTML = null;
+		}
+		document.getElementById('write_in').textContent = document.getElementById('write_in').textContent.substring(0,c_p) + charStr + document.getElementById('write_in').textContent.substring(c_p,document.getElementById('write_in').textContent.length);
+		c_p +=1;
+	}
 };
-(function (global) { 
-    if(typeof (global) === "undefined") {
-        throw new Error("window is undefined");
-    }
-    var _hash = "!";
-    var noBackPlease = function () {
-        global.location.href += "#";
-        global.setTimeout(function () {
-            global.location.href += "!";
-        }, 50);
-    };
 
-    global.onhashchange = function () {
-        if (global.location.hash !== _hash) {
-            global.location.hash = _hash;
-        }
-    };
+function back_space(){
+	if(c_p > 0){
+		document.getElementById('write_in').textContent = document.getElementById('write_in').textContent.substring(0,c_p - 1) + document.getElementById('write_in').textContent.substring(c_p,document.getElementById('write_in').textContent.length );
+		c_p -=1;
+	}
+}
+function replaceNbsps(str) {
+  var re = new RegExp(String.fromCharCode(160), "g");
+  return str.replace(re, " ");
+}
+function allnumeric(str){
+    return !isNaN(parseFloat(str)) && isFinite(str);
+} 
+function enter(){
 
-    global.onload = function () {            
-        noBackPlease();
-        document.body.onkeydown = function (e) {
-            var elm = e.target.nodeName.toLowerCase();
-            if (e.which === 8 && (elm !== 'input' && elm  !== 'textarea')) {
-                e.preventDefault();
-            }
-            e.stopPropagation();
-        };          
-    }
-})(window);
+	errored_word = 0;
+	words = replaceNbsps(document.getElementById('write_in').textContent).split(' ');
+	if(words[0] == "write"){ // write
+		if(words[1] == "-t"){ // to terminal
+			document.getElementById('write_out').innerHTML += "<br>";
+			for(i=2;i<words.length;i++) {
+				document.getElementById('write_out').innerHTML += words[i] + "&nbsp;";
+			}
+		}else{
+			errored_word = 2;
+		}
+	} else if(allnumeric(words[0])){ // number
+		if(words[1] == "+"){ // plus
+			if(allnumeric(words[2])){ // number
+				var a = parseFloat(words[0]) + parseFloat(words[2]);
+				document.getElementById('write_out').innerHTML += "<br>";
+				document.getElementById('write_out').innerHTML += a;
+			}else{
+				errored_word = 3;
+			}
+		}else if(words[1] == "*"){ // multiply
+			if(allnumeric(words[2])){ // number
+				var a = parseFloat(words[0]) * parseFloat(words[2]);
+				document.getElementById('write_out').innerHTML += "<br>";
+				document.getElementById('write_out').innerHTML += a;
+			}else{
+				errored_word = 3;
+			}
+		}else{
+			errored_word = 2;
+		}
+	} else{
+		errored_word = 1;
+	}
+	
+	if(errored_word > 0){
+		document.getElementById('write_out').innerHTML += "<br>" + "'" + words[errored_word -1]+ "'" + "is not recognized as an command" + "<br>";
+	}
+	
+	document.getElementById('write_in').textContent = null;
+	c_p = 0;
+}
+function space(){
+	document.getElementById('write_in').innerHTML = document.getElementById('write_in').textContent.substring(0,c_p) + "&nbsp;" + document.getElementById('write_in').textContent.substring(c_p,document.getElementById('write_in').textContent.length);
+	c_p +=1;
+}
+document.onkeydown = function (event) {
+	
+	if (!event) { /* This will happen in IE */
+		event = window.event;
+	}
+		
+	var keyCode = event.keyCode;
+	
+	if (keyCode == 8 &&
+		((event.target || event.srcElement).tagName != "TEXTAREA") && 
+		((event.target || event.srcElement).tagName != "INPUT")) { 
+		back_space();
+		if (navigator.userAgent.toLowerCase().indexOf("msie") == -1) {
+			event.stopPropagation();
+		} else {
+			alert("prevented");
+			event.returnValue = false;
+		}
+		
+		return false;
+	}
+	if(keyCode == 13){
+		enter();
+	}
+	if(keyCode == 32){
+		space();
+	}
+};	
